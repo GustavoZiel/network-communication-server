cedf1924f1c55c9851f8fdcd6b677706622bcb2c
Gustavo <gugagribeiro@hotmail.com>
2024-06-16 16:01:38 -0300

    Versão 2.0 Aparentemente funcional

--- client.c
+++ client.c
@@ -1,5 +1,3 @@
-// TODO ip -f inet addr show 
-
 #include <arpa/inet.h>
 #include <pthread.h>
 #include <stdio.h>
@@ -18,6 +16,38 @@ int running = 1;
 // Mutex para garantir acesso seguro à variável running
 pthread_mutex_t mutex_running = PTHREAD_MUTEX_INITIALIZER;
 
+int create_socket() {
+    int client_fd;
+    // Criando um servidor IPv4 (AF_INET) com conexão TCP (SOCK_STREAM)
+    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        perror(" [Error] Socket could not be created\n");
+        exit(EXIT_FAILURE);
+    }
+    printf("\n => Socket created with success\n");
+
+    return client_fd;
+}
+
+void config_socket(struct sockaddr_in* serv_addr) {
+    serv_addr->sin_family = AF_INET;
+    serv_addr->sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, IP_ADDRESS, &serv_addr->sin_addr) <= 0) {
+        printf("\n [Error] Invalid address / Address not supported \n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf(" => Socket options set with success\n");
+}
+
+void client_connect(int client_fd, struct sockaddr_in serv_addr) {
+    if (connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\n [Error] Connection Failed \n");
+        exit(EXIT_FAILURE);
+    }
+    printf(" => Connection established with success\n");
+}
+
 void readline(char** str) {
     if (*str != NULL) {
         free(*str);
@@ -33,70 +63,69 @@ void readline(char** str) {
     (*str)[size] = '\0';
 }
 
-void* ler(void* args) {
+void* write_server(void* args) {
     int client_fd = *(int*)args;
-    char buffer[1024];
-    while (1) {
-        pthread_mutex_lock(&mutex_running);
-        if (running == 0) {
-            break;
-        }
-        pthread_mutex_unlock(&mutex_running);
-        memset(buffer, 0, sizeof(buffer));
-        if (read(client_fd, buffer, 1024 - 1) > 0) {
-            printf("%s\n", buffer);
-        }
-    }
-    return NULL;
-}
 
-void* escrever(void* args) {
-    int client_fd = *(int*)args;
     char* buffer = NULL;
     do {
+        // Lê o que o cliente digita
         readline(&buffer);
+        // Envia para o servidor
         send(client_fd, buffer, strlen(buffer), 0);
+        // Até o cliente digitar o comando de saída (/q)
     } while (strcmp(buffer, "/q"));
     free(buffer);
+
     pthread_mutex_lock(&mutex_running);
     running = 0;
     pthread_mutex_unlock(&mutex_running);
     return NULL;
 }
 
+void* read_server(void* args) {
+    int client_fd = *(int*)args;
+    // Tamanho da linha que ele recebe do servidor
+    char buffer[1024];
+
+    while (1) {
+        pthread_mutex_lock(&mutex_running);
+        if (running == 0) {
+            break;
+        }
+        pthread_mutex_unlock(&mutex_running);
+
+        // Cliente recebe mensagens do servidor
+        memset(buffer, 0, sizeof(buffer));
+        if (read(client_fd, buffer, 1024) > 0) {
+            printf("%s\n", buffer);
+        }
+    }
+
+    return NULL;
+}
+
 int main(int argc, char const* argv[]) {
-    printf("\n");
-    
     int status, client_fd;
     struct sockaddr_in serv_addr;
 
-    if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        perror(" [Error] Socket could not be created\n");
-        return EXIT_FAILURE;
-    }
-    printf(" => Socket created with success\n");
+    client_fd = create_socket();
+    config_socket(&serv_addr);
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-    if (inet_pton(AF_INET, IP_ADDRESS, &serv_addr.sin_addr) <= 0) {
-        printf("\n [Error] Invalid address / Address not supported \n");
-        return EXIT_FAILURE;
-    }
+    client_connect(client_fd, serv_addr);
 
-    if ((status = connect(client_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
-        printf("\n [Error] Connection Failed \n");
-        return EXIT_FAILURE;
-    }
-    printf(" => Connection establish with success\n");
+    /*
+    Setando o file_descriptor do client (client_fd) como non-blocking, não bloqueara a
+    execução do programa em operações que causam blocking (read, write). Necessário, visto
+    que o servidor pode ter vários clientes sendo antendidos ao mesmo tempo
+    */
+    // fcntl(client_fd, F_SETFL, O_NONBLOCK);
 
-    fcntl(client_fd, F_SETFL, O_NONBLOCK);
+    pthread_t read_t, write_t;
+    pthread_create(&write_t, NULL, write_server, &client_fd);
+    pthread_create(&read_t, NULL, read_server, &client_fd);
 
-    pthread_t ler_t, escrever_t;
-    pthread_create(&escrever_t, NULL, escrever, &client_fd);
-    pthread_create(&ler_t, NULL, ler, &client_fd);
-
-    pthread_join(escrever_t, NULL);
-    pthread_join(ler_t, NULL);
+    pthread_join(write_t, NULL);
+    pthread_join(read_t, NULL);
 
     close(client_fd);
 
--- server.c
+++ server.c
@@ -1,6 +1,7 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <pthread.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,43 +11,17 @@
 
 #define PORT 8080
 #define MAX_CLIENTS 10
-#define AWAIT_TIME 5
-
+#define AWAIT_TIME 20
 typedef struct client {
     int id;
     char name[20];
     int socket;
+    bool active;
     pthread_t thread;
 } client_t;
 
 client_t clients[MAX_CLIENTS];
 int qtt_clients = 0;
-
-void broadcast_msg(char *msg, int id) {
-    for (int i = 0; i < qtt_clients; i++) {
-        if (i != id) {
-            send(clients[i].socket, msg, strlen(msg), 0);
-        }
-    }
-}
-
-void *send_n_read(void *args) {
-    client_t client = *(client_t *)args;
-    char buffer[512];
-    char msg[1024];
-    while (memset(buffer, 0, sizeof(buffer)) &&
-           read(client.socket, buffer, 500) != 0) {
-        if (!strcmp("/q", buffer)) {
-            snprintf(msg, sizeof(msg), "[%s] saiu do chat.", client.name);
-        } else {
-            snprintf(msg, sizeof(msg), "[%s]: %s", client.name, buffer);
-        }
-        printf("%s\n", msg);
-        broadcast_msg(msg, client.id);
-    }
-    close(client.socket);
-    return NULL;
-}
 
 int create_socket() {
     int server_fd;
@@ -80,7 +55,7 @@ void start_socket(int server_fd, struct sockaddr_in address) {
         perror("[Error] Socket could not be binded");
         exit(EXIT_FAILURE);
     }
-    printf(" => Socket binded to ip [%d] and to port [%d]\n", INADDR_ANY, PORT);
+    printf(" => Socket binded to all interfaces and to port [%d]\n", PORT);
 
     int queue_size = 3;
     if (listen(server_fd, queue_size) < 0) {
@@ -90,56 +65,91 @@ void start_socket(int server_fd, struct sockaddr_in address) {
     printf(" => Socket is listening...\n");
 }
 
-void add_client(int client_socket) {
-    qtt_clients += 1;
-    clients[qtt_clients - 1].id = qtt_clients - 1;
-    clients[qtt_clients - 1].socket = client_socket;
+void broadcast_msg(char *msg, int id) {
+    for (int i = 0; i < qtt_clients; i++) {
+        if (i != id && clients[i].active == true) {
+            send(clients[i].socket, msg, strlen(msg), 0);
+        }
+    }
 }
 
-void register_client(client_t *client) {
+void *send_n_read(int id) {
+    char buffer[512];
+    char msg[1024];
+
+    while (memset(buffer, 0, sizeof(buffer)) && read(clients[id].socket, buffer, 500) != 0) {
+        if (!strcmp("/q", buffer)) {
+            snprintf(msg, sizeof(msg), "[%s] saiu do chat.", clients[id].name);
+            clients[id].active = false;
+            broadcast_msg(msg, clients[id].id);
+            printf("%s\n", msg);
+            break;
+        } else {
+            snprintf(msg, sizeof(msg), "[%s]: %s", clients[id].name, clients[id].id, buffer);
+            broadcast_msg(msg, clients[id].id);
+            printf("%s\n", msg);
+        }
+    }
+    close(clients[id].socket);
+    return NULL;
+}
+
+void register_client(int id) {
     char *welcome_msg = "\n===== WELCOME TO THE CHAT GROUP =====\n";
-    send(client->socket, welcome_msg, strlen(welcome_msg), 0);
+    send(clients[id].socket, welcome_msg, strlen(welcome_msg), 0);
 
     char *register_msg = "What is your name ?";
-    send(client->socket, register_msg, strlen(register_msg), 0);
+    send(clients[id].socket, register_msg, strlen(register_msg), 0);
 
-    // TODO : testar tamanho dos nomes
+    // Getting client name
     char name[20] = {0};
-    int size;
-    size = read(client->socket, name, 19);
-    strcpy(client->name, name);
+    read(clients[id].socket, name, 19);
+    strcpy(clients[id].name, name);
 
-    char *end_msg = "=====================================\n";
-    send(client->socket, end_msg, strlen(end_msg), 0);
-}
+    char *end_msg = "=====================================\n\n";
+    send(clients[id].socket, end_msg, strlen(end_msg), 0);
 
-void *handle_client(void *args) {
-    client_t *client = (client_t *)args;
-    register_client(client);
-    send_n_read(client);
-    return NULL;
+    char *chat_start = "Chat:\n";
+    send(clients[id].socket, chat_start, strlen(chat_start), 0);
+
+    clients[id].active = true;
 }
 
 void get_info(int client) {
     struct sockaddr_in client_addr;
+
     socklen_t client_addr_len = sizeof(client_addr);
     getpeername(client, (struct sockaddr *)&client_addr, &client_addr_len);
 
     char client_ip[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &client_addr.sin_addr, client_ip, INET_ADDRSTRLEN);
 
-    printf("======== Connection %d =========\n", qtt_clients + 1);
+    printf("\n======== Connection %d =========\n", qtt_clients + 1);
     printf("Client IP address: %s\n", client_ip);
     printf("Client port: %d\n", ntohs(client_addr.sin_port));
-    printf("===============================\n");
+    printf("===============================\n\n");
+}
+
+void add_client(int client_socket) {
+    clients[qtt_clients].id = qtt_clients;
+    clients[qtt_clients].socket = client_socket;
+    qtt_clients += 1;
+}
+
+void *handle_client(void *args) {
+    int *id = (int *)args;
+    int id_client = *id;
+    register_client(id_client);
+    send_n_read(id_client);
+    return NULL;
 }
 
 void handle_server(int server_fd, struct sockaddr_in address) {
     fd_set readfds;
-    int client_socket;
+    int client_socket, id_client;
     socklen_t addrlen = sizeof(address);
 
-    // Wait for activity on any of the sockets with timeout
+    // Creating timeout object
     struct timeval *timeout = NULL;
     if (AWAIT_TIME != 0) {
         timeout = malloc(sizeof(struct timeval));
@@ -159,17 +169,13 @@ void handle_server(int server_fd, struct sockaddr_in address) {
         FD_SET(server_fd, &readfds);
         int max_sd = server_fd;
 
-        // To use the timeout
+        // Wait for activity on any of the sockets with timeout
         int activity = select(max_sd + 1, &readfds, NULL, NULL, timeout);
-
-        // Not use timeout (server runs unstopping)
-        // int activity = select(max_sd + 1, &readfds, NULL, NULL, NULL);
 
         if (activity == -1) {
             perror("[Error] Wainting for sockets activity failed");
             exit(EXIT_FAILURE);
         } else if (activity == 0) {
-            // TODO : Testar isso aqui, roda até ficar inativo por time out ?
             // Timeout occurred, exit loop
             break;
         }
@@ -184,9 +190,10 @@ void handle_server(int server_fd, struct sockaddr_in address) {
 
             get_info(client_socket);
             add_client(client_socket);
+            id_client = qtt_clients - 1;
 
             // Create thread to handle new connection
-            if (pthread_create(&clients[qtt_clients - 1].thread, NULL, handle_client, (void *)&clients[qtt_clients - 1]) != 0) {
+            if (pthread_create(&clients[qtt_clients - 1].thread, NULL, handle_client, (void *)&id_client) != 0) {
                 perror("[Error] Creation of a thread for a client failed");
                 close(client_socket);
             }
@@ -194,12 +201,12 @@ void handle_server(int server_fd, struct sockaddr_in address) {
     }
     free(timeout);
 
-    printf("\n==== The socket is closed for futher connections ====\n");
+    printf("==== The socket is closed for futher connections ====\n");
 
     // Wait for each thread to finish
     for (int i = 0; i < qtt_clients; i++) {
         if (pthread_join(clients[i].thread, NULL) != 0) {
-            perror("[Error] Waiting thread to finish failed");
+            perror("[Error] Waiting for threads to finish");
             exit(EXIT_FAILURE);
         }
     }
@@ -208,12 +215,15 @@ void handle_server(int server_fd, struct sockaddr_in address) {
 int main(int argc, char const *argv[]) {
     int server_fd;
     server_fd = create_socket();
-
     config_socket(server_fd);
 
     struct sockaddr_in address;
     set_address(&address);
     start_socket(server_fd, address);
+
+    printf("\n=================================\n");
+    printf("\tThe socket is on\t\n");
+    printf("=================================\n");
 
     handle_server(server_fd, address);
 
